e908e9dfe8f3376e8f8ce1dc37b2d0cad105b1b9
gaven <gaven@F>
2023-07-23 16:02:35 +0800

    添加项目文件。

---
+++ console/Utils/SqlHelper.cs
@@ -0,0 +1,13 @@
+﻿using SqlSugar;
+
+namespace console.Utils;
+
+public class SqlHelper
+{
+	public static SqlSugarClient GetClient() => new SqlSugarClient(new ConnectionConfig()
+	{
+		ConnectionString = @"DataSource=data.sqlite",
+		DbType = DbType.Sqlite,
+		IsAutoCloseConnection = true
+	});
+}
