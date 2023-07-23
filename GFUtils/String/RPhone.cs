fee336dd0e330fee29590900cdd775c1613f81b6
fallen gaven <gavenf2023@outlook.com>
2023-07-23 16:35:48 +0800

    更改解决方案名称

--- GFUtils/String/RPhone.cs
+++ GFUtils/String/RPhone.cs
@@ -4,14 +4,25 @@ namespace GFUtils.String;
 
 public static class RPhone
 {
-	private static readonly System.Text.RegularExpressions.Regex PhoneRegex =
+	private static readonly Regex PhoneRegex =
 		new(@"1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}");
 
-	private static readonly System.Text.RegularExpressions.Regex PhoneRegexAlone =
+	private static readonly Regex PhoneRegexAlone =
 		new(@"^1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}$");
 
 
-	public static bool IsPhone(this string data) => PhoneRegexAlone.IsMatch(data);
-	public static Match MatchPhone(this string data) => PhoneRegex.Match(data);
-	public static string MatchFirstPhone(this string data) => MatchPhone(data).Groups[0].Value;
+	public static bool IsPhone(this string data)
+	{
+		return PhoneRegexAlone.IsMatch(data);
+	}
+
+	public static Match MatchPhone(this string data)
+	{
+		return PhoneRegex.Match(data);
+	}
+
+	public static string MatchFirstPhone(this string data)
+	{
+		return MatchPhone(data).Groups[0].Value;
+	}
 }
