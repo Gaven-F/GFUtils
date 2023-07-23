143218e3a44365fe276ffa05cca30986cb7cd414
fallen gaven <gavenf2023@outlook.com>
2023-07-23 16:24:33 +0800

    添加邮箱验证

---
+++ GFUtils/String/REmail.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GFUtils.String;
+
+public static class REmail
+{
+	private static readonly Regex EmailRegex =
+		new(
+			@"[A-Za-z0-9!#$%&'+/=?^_`{|}~-]+(.[A-Za-z0-9!#$%&'+/=?^_`{|}~-]+)*@([A-Za-z0-9]+(?:-[A-Za-z0-9]+)?.)+[A-Za-z0-9]+(-[A-Za-z0-9]+)?");
+
+	private static readonly Regex EmailRegexAlone =
+		new(@"^[A-Za-z0-9!#$%&'+/=?^_`{|}~-]+(.[A-Za-z0-9!#$%&'+/=?^_`{|}~-]+)*@([A-Za-z0-9]+(?:-[A-Za-z0-9]+)?.)+[A-Za-z0-9]+(-[A-Za-z0-9]+)?$");
+
+
+	public static bool IsEmail(this string data)
+	{
+		return EmailRegexAlone.IsMatch(data);
+	}
+
+	public static Match MatchEmail(this string data)
+	{
+		return EmailRegex.Match(data);
+	}
+
+	public static string MatchFirstEmail(this string data)
+	{
+		return MatchEmail(data).Groups[0].Value;
+	}
+}
